af098f5fd850266d5ea21e6fad0c1551a36cff14
Gabriel-Alloza <gabriel.alloza@le-campus-numerique.fr>
2025-05-20 11:16:54 +0200

    Ajout de la page contact (vide)

---
+++ contact.php
@@ -0,0 +1,8 @@
+<?php
+    $titre = "Contact";
+    $navbar = "<p><a href = 'index.php'>Accueil</a><a href = 'gallerie.php'>Gallerie</a>Contact</p>";
+    $texte_entete = "Contactez-nous!";
+    include 'header.php';
+?>
+
+<?php include 'footer.php'; ?>
