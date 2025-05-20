7fd02772e9cae45725fe31a67f5d23236666e455
Gabriel-Alloza <gabriel.alloza@le-campus-numerique.fr>
2025-05-20 11:13:38 +0200

    Ajout de la gallerie (vide)

---
+++ gallerie.php
@@ -0,0 +1,6 @@
+<?php
+    $titre = "Gallerie";
+    $navbar = "<p><a href = 'index.php'>Accueil</a>Gallerie<a href = 'contact.php'>Contact</a></p>";
+    $texte_entete = "Bienvenue dans la gallerie!";
+    include 'header.php';
+?>
