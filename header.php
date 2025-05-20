d668da0b383b9d2d642855e1240834d33ea0ecbd
Gabriel-Alloza <gabriel.alloza@le-campus-numerique.fr>
2025-05-20 11:55:22 +0200

    Ajout du style dans le head et de la balise main

--- header.php
+++ header.php
@@ -1,6 +1,7 @@
 <html>
     <head>
         <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+        <link rel="stylesheet" href="style.css">
         <title><?php echo $titre; ?></title>
         <link rel="icon" type="image/x-icon" href= "Images/favicon.jpg">
 
@@ -18,11 +19,13 @@
                 </div>
             </div>
 
-            <div class = "row">
-                <div class = "col-sm-12">
-                    <h1><?php echo $texte_entete; ?></h1>
+            <main>
+
+                <div class = "row">
+                    <div class = "col-sm-12">
+                        <h1><?php echo $texte_entete; ?></h1>
+                    </div>
                 </div>
             </div>
-        </div>
 
         
