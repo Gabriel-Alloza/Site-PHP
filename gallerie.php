eb679820507cc45c295f80c01ffd7c0c4c274bea
Gabriel-Alloza <gabriel.alloza@le-campus-numerique.fr>
2025-05-20 11:50:04 +0200

    Mise en forme des images

--- gallerie.php
+++ gallerie.php
@@ -5,9 +5,23 @@
     include 'header.php';
 ?>
 
-<img src = "Images/imm1.jpg" alt = "Andor 1">
-<img src = "Images/imm2.webp" alt = "Andor 2">
-<img src = "Images/imm3.webp" alt = "Andor 3">
+<div class="container">
+    <div class = "row">
+        <div class = "col-sm-4">
+            <img src = "Images/imm1.jpg" alt = "Andor 1" width="300" height="600">
+        </div>
+
+
+        <div class = "col-sm-4">
+            <img src = "Images/imm2.webp" alt = "Andor 2"  width="400" height="200">
+        </div>
+
+        <div class = "col-sm-4">
+            <img src = "Images/imm3.webp" alt = "Andor 3" width="300" height="600">
+        </div>
+
+    </div>
+</div>
 
 
 <?php include 'footer.php'; ?>
