698c4b18cf80bc21c65b1aadfa9db801daf2ad73
Gabriel-Alloza <gabriel.alloza@le-campus-numerique.fr>
2025-05-20 11:43:06 +0200

    Ajout des images

--- gallerie.php
+++ gallerie.php
@@ -5,4 +5,9 @@
     include 'header.php';
 ?>
 
+<img src = "Images/imm1.jpg" alt = "Andor 1">
+<img src = "Images/imm2.webp" alt = "Andor 2">
+<img src = "Images/imm3.webp" alt = "Andor 3">
+
+
 <?php include 'footer.php'; ?>
