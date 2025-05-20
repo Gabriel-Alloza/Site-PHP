a54148042fda993d2e5f6393057d76d52ebdba4d
Gabriel-Alloza <gabriel.alloza@le-campus-numerique.fr>
2025-05-20 09:35:05 +0200

    Ajout du footer, formating bootstrap à faire plus tard

---
+++ footer.php
@@ -0,0 +1,7 @@
+<footer>
+    <p>Ce site a été fait par Gabriel Alloza</p>
+</footer>
+
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"></script>
+
+</html>
