f208992ba5cfb891d72ef0c3a20d51c4ba73226e
Gabriel-Alloza <gabriel.alloza@le-campus-numerique.fr>
2025-05-20 09:46:07 +0200

    Ajout de l'index, (style à faire plus tard)

---
+++ index.php
@@ -0,0 +1,10 @@
+<?php
+    $titre = "Accueil";
+    $navbar = "<p>Accueil</p><a href = 'gallerie.php'>Gallerie</a><a href = 'contact.php'>Contact</a>";
+    $texte_entete = "Bonjour et bienvenue sur mon site!";
+    include 'header.php';
+?>
+
+<p>Vous vous trouvez sur la page d'accueil de mon site, il s'agit d'un exercice de PHP donc il n'y a pas grand chose ici</p>
+
+<?php include 'footer.php'; ?>
