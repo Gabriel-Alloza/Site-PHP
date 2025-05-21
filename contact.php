2cafcb69fab0a6423497452d3f043aa7124de3a3
Gabriel-Alloza <gabriel.alloza@le-campus-numerique.fr>
2025-05-21 10:29:43 +0200

    complétion formulaire (WIP)

--- contact.php
+++ contact.php
@@ -5,43 +5,47 @@
     include 'header.php';
 ?>
 
-<form action = "/contact.php">
+<form action = "contact.php" mehod = "post">
     <select>
         <option>Homme</option>
         <option>Femme</option>
     </select>
     <br>
     <br>
-    Nom: <input type = "text">
+    Nom: <input type = "text" name = "nom">
     <br>
     <br>
-    Prénom: <input type = "text">
+    Prénom: <input type = "text" name = "prénom">
     <br>
     <br>
-    email: <input type = "text">
+    email: <input type = "text" name = "email">
     <br>
     <br>
     Raison du contact: 
     <br>
-    <input type = "radio">
+    <input type = "radio" name = "raison_contact">
     <label>A</label>
     <br>
     <br>
-    <input type = "radio">
+    <input type = "radio" name = "raison_contact">
     <label>B</label>
     <br>
     <br>
-    <input type = "radio">
+    <input type = "radio" name = "raison_contact">
     <label>C</label>
     <br>
     <br>
-    Message: <textarea></textarea>
+    Message: <textarea name = "message"></textarea>
     <br>
     <br>
     <input type = "submit" value = "Envoyer">
 
 
 </form>
+
+<?php
+    echo $_POST["message"];
+ ?>
 
 
 <?php include 'footer.php'; ?>
